415c28b8658c61c7055978a2b6a31fb1ccd92391
Craig Norton <c9952594@gmail.com>
2017-03-01 01:18:02 +0000

    Doen the first three problems. On the fourth now.

---
+++ Rosalind/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Rosalind {
+    sealed partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Output.Location = new System.Drawing.Point(0, 0);
+            this.Output.Name = "Output";
+            this.Output.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.Output.Size = new System.Drawing.Size(569, 523);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 523);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Output;
+    }
+}
+
---
+++ Rosalind/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Rosalind {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            AllowDrop = true;
+            DragEnter += Form1_DragEnter;
+            DragDrop += Form1_DragDrop;
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        readonly Dictionary<string, Func<string, string>> _problems = new Dictionary<string, Func<string, string>> {
+            ["rosalind_dna"] = CountingDnaNucleotides,
+            ["rosalind_rna"] = TranscribingDnaIntoRna,
+            ["rosalind_revc"] = ComplementingAStrandOfDna,
+            ["rosalind_fib"] = RabbitsAndRecurrenceRelations
+        };
+
+        void Form1_DragDrop(object sender, DragEventArgs e) {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files) {
+                var content = File.ReadAllText(file).Trim();
+                var filename = Path.GetFileNameWithoutExtension(file);
+                var problem = _problems.First(m => filename.StartsWith(m.Key)).Value;
+                Output.Text = problem(content);
+            }
+        }
+
+        private static string CountingDnaNucleotides(string content) {
+            // http://rosalind.info/problems/dna/
+            return
+                content
+                    .ToCharArray()
+                    .OrderBy(m => m)
+                    .GroupBy(m => m)
+                    .Select(m => m.Count())
+                    .Aggregate("", (result, count) => result + count + " ")
+                    .TrimEnd();
+        }
+
+        private static string TranscribingDnaIntoRna(string content) {
+            // http://rosalind.info/problems/rna/
+            return content.Replace('T', 'U');
+        }
+
+        private static string ComplementingAStrandOfDna(string content) {
+            // http://rosalind.info/problems/revc/
+            var compliment = new Dictionary<char, char>() {
+                ['A'] = 'T',
+                ['T'] = 'A',
+                ['C'] = 'G',
+                ['G'] = 'C'
+                
+            };
+            return new string(content.ToCharArray().Select(c => compliment[c]).Reverse().ToArray());
+        }
+
+        private static string RabbitsAndRecurrenceRelations(string content) {
+            // http://rosalind.info/problems/fib/
+            return "";
+        }
+    }
+}
